b6f9706cc4c827545421484644c42cd10164f911
DESKTOP-8GQPUE5\datacom <iampatwari@gmail.com>
2023-04-09 17:53:28 +1200

    Upd: added try catch block

--- src/pages/api/PostGuid.js
+++ src/pages/api/PostGuid.js
@@ -4,14 +4,18 @@ import path from "path";
 export default function handler(req, res) {
   // Get the new GUID from the request body
   if (req.method === "POST") {
-    const { guid } = req.body;
-    console.log(typeof guid);
-    // Update the Guid.json file with the new GUID
-    const filePath = path.join(process.cwd(), "Guid.json");
-    const fileData = { guid };
-    fs.writeFileSync(filePath, JSON.stringify(fileData));
+    try {
+      const { guid } = req.body;
+      console.log(typeof guid);
+      // Update the Guid.json file with the new GUID
+      const filePath = path.join(process.cwd(), "Guid.json");
+      const fileData = { guid };
+      fs.writeFileSync(filePath, JSON.stringify(fileData));
 
-    // Return a success response
-    res.status(200).json({ guid });
+      // Return a success response
+      res.status(200).json({ guid });
+    } catch (error) {
+      res.send(error);
+    }
   }
 }
