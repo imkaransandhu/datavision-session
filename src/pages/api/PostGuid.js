25c1abf0b1d28bc71a8006204c51b96b632848b7
DESKTOP-8GQPUE5\datacom <iampatwari@gmail.com>
2023-04-09 17:49:29 +1200

    Upd: added if fot post

--- src/pages/api/PostGuid.js
+++ src/pages/api/PostGuid.js
@@ -1,20 +1,17 @@
 import fs from "fs";
 import path from "path";
-export const config = {
-  api: {
-    responseLimit: "8mb",
-    bodyParser: false,
-  },
-};
+
 export default function handler(req, res) {
   // Get the new GUID from the request body
-  const { guid } = req.body;
-  console.log(typeof guid);
-  // Update the Guid.json file with the new GUID
-  const filePath = path.join(process.cwd(), "Guid.json");
-  const fileData = { guid };
-  fs.writeFileSync(filePath, JSON.stringify(fileData));
+  if (req.method === "POST") {
+    const { guid } = req.body;
+    console.log(typeof guid);
+    // Update the Guid.json file with the new GUID
+    const filePath = path.join(process.cwd(), "Guid.json");
+    const fileData = { guid };
+    fs.writeFileSync(filePath, JSON.stringify(fileData));
 
-  // Return a success response
-  res.status(200).json({ guid });
+    // Return a success response
+    res.status(200).json({ guid });
+  }
 }
